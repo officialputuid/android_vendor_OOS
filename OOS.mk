786e1ca1d54895041b9093f62736f9cf9141eab4
MrSluffy <werdna.jac@gmail.com>
2021-08-27 10:24:11 +0800

    OOS: Add OPFileManager - OPFileManager Port v2.6.4.0.201116183715.d0f80a3 - Port from https://t.me/bugreport_group (source from here)
    Signed-off-by: officialputuid <officialputuid@hack.id>

--- OOS.mk
+++ OOS.mk
@@ -2,11 +2,13 @@ include $(call first-makefiles-under,$(LOCAL_PATH))
 
 PRODUCT_COPY_FILES += \
     vendor/OOS/etc/permissions/privapp-permissions-op-launcher.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-op-launcher.xml \
+    vendor/OOS/etc/permissions/privapp-permissions-op-filemanager.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-op-filemanager.xml \
     vendor/OOS/etc/sysconfig/op-launcher-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/op-launcher-hiddenapi-package-whitelist.xml
 
 PRODUCT_PACKAGES += \
     OPCamera \
     OPDeskClock \
+    OPFileManager \
     OPIconpackOxygen \
     OPIconpackRound \
     OPRecentsProvider \
